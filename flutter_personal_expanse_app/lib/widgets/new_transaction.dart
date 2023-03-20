ac7b393b6b7c8b119aed7b64d2f95c01acba513c
ko hunmin <khmsoul@gmail.com>
2023-03-20 22:56:58 +0900

    Update new_transaction.dart

--- flutter_personal_expanse_app/lib/widgets/new_transaction.dart
+++ flutter_personal_expanse_app/lib/widgets/new_transaction.dart
@@ -10,6 +10,20 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addNewTransaction(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -29,6 +43,8 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
               // onChanged: (val) {
               //   amountInput = val;
               // },
@@ -38,12 +54,7 @@ class NewTransaction extends StatelessWidget {
               style: TextButton.styleFrom(
                 foregroundColor: Colors.purple,
               ),
-              onPressed: () {
-                addNewTransaction(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
             ),
           ],
         ),
